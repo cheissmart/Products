f41f41b451b43125eaed85da2375f69f523d7bcb
cheissmart <cheissmart@gmail.com>
2018-03-22 22:23:36 +0800

    Update toBinary.cpp

--- toBinary.cpp
+++ toBinary.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 void printBin(int n) {
     cout << "The binary of " << n << " is "; 
-    int mask = 1 << 31;
-    for(int i=0;i<32;i++) {
+    int mask = 1 << 30;
+    for(int i=0;i<31;i++) {
         cout << ((mask & n) ? 1 : 0);
         mask >>= 1;
     }
